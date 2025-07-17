9ff0b8f12a3602b73abf7a70f51ea5de56ac6701
SafalyaRC <safalyaroy9463@gmail.com>
2025-07-17 20:15:05 +0530

    Context API

---
+++ 08-context-api/src/context/UserContextProvider.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserContext from "./UserContext";
+import { useState } from "react";
+
+const UserContextProvider = ({ children }) => {
+    const [user,setUSer]=useState(null)
+    return (
+        <UserContext.Provider value={{user,setUSer}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider;
